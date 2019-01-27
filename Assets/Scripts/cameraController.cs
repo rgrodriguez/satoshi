4e5f61edb36288995b3f76b42489bc1074f9cbfd
Ramon Rodriguez <rgrodriguez94@gmail.com>
2019-01-26 23:08:52 -0600

    Added changables

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -8,13 +8,11 @@ public class cameraController : MonoBehaviour {
 
     private Vector3 offset;
 
-    // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
