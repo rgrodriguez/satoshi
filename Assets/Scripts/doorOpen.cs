4d624da330ea9180f388783acb0aa1315bde706d
Ramon Rodriguez <rgrodriguez94@gmail.com>
2019-01-27 11:28:53 -0600

    Door Opening script still not working

---
+++ Assets/Scripts/doorOpen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen : MonoBehaviour
+{
+    public bool enter;
+    private AudioSource source;
+    int count = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enter = false;
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+        if (enter && count == 1)
+        {
+            source.Play();
+            count -= 1;
+            Debug.Log("HELLO");
+        }
+    }
+}
